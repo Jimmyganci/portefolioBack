d592069e38fe7b213c160c3fceb67208f4ba44c5
Jimmy <jimmy.ganci@gmail.com>
2021-12-20 13:35:40 +0100

    changed procfile

--- app.js
+++ app.js
@@ -16,39 +16,35 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.post("/send", (req, res) => {
+app.post("/", (req, res) => {
   const { firstname, lastname, email, subject, text } = req.body;
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "jimmy.ganci@gmail.com",
-        pass: process.env.PWD_GMAIL,
-      },
-    });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "jimmy.ganci@gmail.com",
+      pass: process.env.PWD_GMAIL,
+    },
+  });
 
-    const mailOptions = {
-      from: "myPortefolio",
-      to: "jimmy.ganci@gmail.com",
-      subject: subject,
-      text: `Vous avez reçu un mail de: ${email}!
+  const mailOptions = {
+    from: "myPortefolio",
+    to: "jimmy.ganci@gmail.com",
+    subject: subject,
+    text: `Vous avez reçu un mail de: ${email}!
 	Prénom:${firstname}
 	Nom:${lastname}
 	Sujet:${subject}
 	Message: ${text}`,
-    };
+  };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
-    res.status(200).send();
-  } catch (err) {
-    res.status(404).send(err);
-  }
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+  res.status(200).send();
 });
 
 app.listen(port, () => {
